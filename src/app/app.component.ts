a391f7d6d5dbcccf00fbcbafec157bc13529506f
jmbohan <jmbohan@purdue.edu>
2022-03-24 10:32:17 -0400

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component, VERSION } from '@angular/core';
+import { of, from } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  ngOnInit() {
+    of(2, 4, 6, 8).subscribe((item) => console.log(item));
+
+    from([20, 15, 10, 5]).subscribe({
+      next: (item) => console.log(`resulting item.. ${item}`),
+      error: (err) => console.error(`error occurred ${err}`),
+      complete: () => console.log(`complete`),
+    });
+  }
 }
